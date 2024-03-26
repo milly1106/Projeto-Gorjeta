a50a8554d4a7bd95f6f439f7255442b10dbd0039
millysantoss <millysantoss1106@gmail.com>
2024-03-26 12:46:10 -0300

    Projeto gorjeta

---
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button15Percent_Clicked(object sender, EventArgs e)
+
+        {
+            PercentagemSlider.Value = 15;
+        }
+        private void Button30percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+            double tip = CalculateTip();
+            tip = Math.Ceiling(tip);
+            TipLabel.Text = Convert.ToString(tip);
+            double valorRefeicao = tip + Convert.ToDouble(ValueEntry.Text);
+            TotaLabel.Text = valorRefeicao.ToString("C2");
+        }
+        private void ButtonRoundDown_Clicked(object sender, EventArgs e)
+        {
+         
+        }
+
+        private double CalculateTip()
+        {
+            double valRefeicao = Convert.ToDouble(ValueEntry.Text);
+            double valporcentagem = PercentagemSlider.Value;
+            double valorGorjeta = valRefeicao * (valporcentagem / 100);
+            return (valorGorjeta);
+
+        }
+
+        private void PercentagemSlider_ValueChanged(object sender, EventArgs e) {
+
+            PorcentagemLabel.Text = $"{PercentagemSlider.Value.ToString()}%";
+
+            
+
+            }
+    }
+}
